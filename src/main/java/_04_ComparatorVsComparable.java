071db408c5943ae889b86108ac16f6ca9c509740
Ayush <ayushojha79@gmail.com>
2025-04-26 14:08:14 +0530

    Q4: What is the difference between comparable an comparator interface

---
+++ src/main/java/_04_ComparatorVsComparable.java
@@ -0,0 +1,20 @@
+import support.problem4.UsingComparable;
+import support.problem4.UsingComparator;
+
+public class _04_ComparatorVsComparable {
+
+//    Question : Explain internal working of HashMap ?
+//    Explanation :
+
+///     | Features          |          Comparable                        |          Comparator                      |
+///     | Definition        | defines natural ordering within the class  | It defines external sorting logic.       |
+///     | Method            |        compareTo()                         | compare()                                |
+///     | Implementation    | It is implemented in the class.            | It is implemented in a separate class.   |
+///     | Sorting Criteria  | Natural order sorting                      | Custom sorting                           |
+///     | Usage             | It is used for a single sorting order.     | It is used for multiple sorting orders.  |
+    public static void main(String[] args) {
+        UsingComparable.printSortedList();
+        System.out.println("===============================================================");
+        UsingComparator.printSortedList();
+    }
+}
---
+++ src/main/java/support/problem4/RatingComparator.java
@@ -0,0 +1,10 @@
+package support.problem4;
+
+import java.util.Comparator;
+
+public class RatingComparator implements Comparator<Movie1> {
+    @Override
+    public int compare(Movie1 o1, Movie1 o2) {
+        return o2.getRating().compareTo(o1.getRating());
+    }
+}
